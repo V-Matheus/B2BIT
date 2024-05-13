27f2bc8d97a32782dc7caded04de98adaa2545e1
Victor Matheus <victormatheus507@gmail.com>
2024-05-13 19:40:43 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './App.css'
+
+const App: React.FC = () => {
+  return (
+    <>
+      <h1 className='text-red-800'>Hello Word</h1>
+    </>
+  )
+}
+
+export default App
