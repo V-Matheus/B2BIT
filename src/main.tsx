b7645ae8253741cc52fdbdc21ef7e3ffa0409b5c
Victor Matheus <victormatheus507@gmail.com>
2024-05-14 16:06:07 -0300

    configured routes

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,22 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './styles/index.css'
 import './styles/globalStyles.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Profile from './routes/Profile.tsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />
+  }, 
+  {
+    path: '/profile',
+    element: <Profile />
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
